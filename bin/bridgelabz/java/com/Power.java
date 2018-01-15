b614ab116fba2565207acea3f11c42406521bb7e
Sana Shaikh <sa28na@gmail.com>
2018-01-15 11:00:21 +0530

    third commit

--- bin/bridgelabz/java/com/Power.java
+++ bin/bridgelabz/java/com/Power.java
@@ -1,27 +1,22 @@
 import java.util.*;
-
-public class task1d
+public class Power
 {
      public static void main(String[] args)
      {
-            Scanner sc=new Scanner(System.in);
-	    System.out.println("Enter power value N:");
-	    int power=sc.nextInt();
-
-	    
-          
- 	    System.out.println("Powers of 2 that is less than 2^");
-	    int n=sc.nextInt();
-
-            int i = 0;
-           // int power = 1;
+    	Scanner sc=new Scanner(System.in);
+ 	System.out.println("Enter power value N:");
+ 	int n=sc.nextInt();
  
-           
-            while (i <= n)
-            {
-                System.out.println("2^"+i+" = " + power);
-                power = 2^ power;
-                i++; 
-            }
-        }
+        int i = 0;
+        int power = 1;
+ 
+         System.out.println("Powers of 2 that are less than 2^"+n);
+         while (i <= n)
+         {
+             System.out.println("2^"+i+" = " + power);
+             power = power * 2;
+             i++;
+          }
+      }
 }
+
